9890292f5c4a1fe659975e45db580b053406f8a2
kimbwook <bwook00@naver.com>
2024-05-21 16:08:03 +0900

    delete llm parameter

--- tests/autorag/nodes/passagecompressor/test_longllmlingua.py
+++ tests/autorag/nodes/passagecompressor/test_longllmlingua.py
@@ -1,9 +1,26 @@
+from typing import List
+
 from autorag.nodes.passagecompressor import longllmlingua
-from tests.autorag.nodes.passagecompressor.test_base_passage_compressor import (queries, retrieved_contents,
-                                                                                check_result)
+
+queries = [
+    "What is the capital of France?",
+    "What is the meaning of life?",
+]
+retrieved_contents = [
+    ["Paris is the capital of France.", "France is a country in Europe.", "France is a member of the EU."],
+    ["The meaning of life is 42.", "The meaning of life is to be happy.", "The meaning of life is to be kind."],
+]
 
 
 def test_longllmlingua_default():
     result = longllmlingua.__wrapped__(queries, retrieved_contents, [], [])
     print(result)
     check_result(result)
+
+
+def check_result(result: List[str]):
+    assert len(result) == len(queries)
+    for r in result:
+        assert isinstance(r, str)
+        assert len(r) > 0
+        assert bool(r) is True
